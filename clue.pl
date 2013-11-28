58650d86942f6875fc2160292fbac2a06377f777
Edward Soo <paktiksoo@gmail.com>
2013-11-27 22:12:20 -0800

    better setup routine

--- clue.pl
+++ clue.pl
@@ -35,15 +35,17 @@ valid_card(X) :- room(X).
 % Game setup
 clue :- write('Welcome!'),nl.
 
-% the number of players
-:-dynamic players/1.
-
 % my cards
 :-dynamic mycards/1.
-:-dynamic menu_opt/1.
+
+% played_char(X) is true if character is being played as by a player
+:-dynamic my_char/1.
+:-dynamic played_char/1.
 
 % next_player(X,Y) is true if X is the player to the immediate left of Y
 :-dynamic next_player/2.
+% This predicate is used to save the last user input
+:-dynamic last_input/1.
 
 % Returns the index (1-based) of an element in a list, -1 if element is not in the list
 index_of(X, List, -1) :- not(member(X, List)), !.
@@ -53,29 +55,52 @@ index_of_no_fail(X, [_|Tl], N_Index) :- index_of_no_fail(X, Tl, Index), N_Index 
 
 % Returns a list of tuple {index, element} with index starting at Start
 index_zip([], [], _).
-index_zip([Hd|Tl], [[Start,Hd]|Tuples], Start) :- N_Start is Start + 1, index_zip(Tl, Tuples, N_Start).
+index_zip([Hd|Tl], [[Start,Hd]|Ts], Start) :- N_Start is Start + 1, index_zip(Tl, Ts, N_Start).
 
 
 % Game setup menu
-record_num_of_players :- write('How many players are there?'),nl,
-	read(N), N > 0, N < 7, assert(player_num(N)).
+setup :- cleanup, record_my_char, record_played_char, record_order.
 
-record_my_player :- write('Which character are you playing?'), nl,
+read_input(Input) :- retractall(last_input(_)), read(Input), assert(last_input(Input)).
+
+cleanup :- retractall(played_char(_)),
+  retractall(next_player(_,_)),
+  retractall(my_char(_)),
+  retractall(last_input(_)).
+
+record_my_char :- write('Which character are you playing?'), nl,
   findall(C, character(C), Cs),
-  index_zip(Cs, Tuples, 0),
-  foreach(member(Tuple, Tuples), writef("%d. %d\n", Tuple)),
+  index_zip(Cs, Ts, 0),
+  foreach(member(T, Ts), writef("%d. %d\n", T)),
   read(N), N > -1, N < 6, nth0(N, Cs, My_C),
-  assert(my_character(My_C)).
+  assert(my_char(My_C)), assert(played_char(My_C));
+  write('Not a valid option'), nl, record_my_char.
 
-record_order :- foreach((character(C), not(next_player(_, C))), record_next_player(C)).
+record_played_char :- write('Which other character is being played?'), nl,
+  findall(C, (character(C), not(played_char(C))), Not_played),
+  length(Not_played, Num_choices),
+  index_zip(Not_played, Ts, 0),
+  foreach(member(T, Ts), writef("%d. %d\n", T)),
+  writef("%d. None of the above\n", [Num_choices]),
+  read_input(N), N > -1, N < Num_choices, nth0(N, Not_played, Played),
+  assert(played_char(Played)), record_played_char;
+  last_input(N), !, findall(C, (character(C), not(played_char(C))), Not_played),
+  length(Not_played, Num_choices), Num_choices =:= N;
+  write('Not a valid option'), nl, record_played_char.
 
-record_next_player(P) :- writef('Who is the player to the left of %d?', [P]), nl,
-  findall(C, (character(C), not(next_player(C, _)), C \== P), No_prevs),
-  length(No_prevs, Num_choice),
-  index_zip(No_prevs, Tuples, 0),
-  foreach(member(Tuple, Tuples), writef("%d. %d\n", Tuple)),
-  read(N), N > -1, N < Num_choice, nth0(N, No_prevs, Next),
-  assert(next_player(Next, P)).
+record_order :- foreach((played_char(C), not(next_player(_, C))), record_next_player(C)).
+record_next_player(P) :- findall(C, (played_char(C), not(next_player(C, _)), C \== P), No_prevs),
+  length(No_prevs, Num_choices),
+  Num_choices =\= 1,
+  writef('Who is the player to the left of %d?', [P]), nl,
+  index_zip(No_prevs, Ts, 0),
+  foreach(member(T, Ts), writef("%d. %d\n", T)),
+  read(N), N > -1, N < Num_choices, nth0(N, No_prevs, Next),
+  assert(next_player(Next, P));
+  findall(C, (played_char(C), not(next_player(C, _)), C \== P), No_prevs),
+  length(No_prevs, Num_choices), Num_choices =:= 1,
+  played_char(C), not(next_player(C, _)), !, assert(next_player(C, P));
+  write('Not a valid option'), nl, record_order.
 
 record_my_cards :- write('What are your cards?'),nl,
 	read(C), valid_card(C),assert(mycards(C)), record_my_cards;
